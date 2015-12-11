185b5fa618d858da67de543c4591ce0cff9f74ec
Johannes Stein <johannesstein@freeze-dev.com>
2015-12-11 13:58:43 +0100

    Use image

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,20 +3,17 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+import { yeoman } from 'images';
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
+        <img src={yeoman} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
     );
   }
 }
-
-AppComponent.defaultProps = {
-};
 
 export default AppComponent;
